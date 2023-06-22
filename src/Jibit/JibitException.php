00ad0780a9e431280d15f2e4761c9e87435f236a
redcoder <mzero651@gmail.com>
2023-06-22 22:20:12 +0330

    Add purchase.forbidden_card_number message and code

--- src/Jibit/JibitException.php
+++ src/Jibit/JibitException.php
@@ -44,6 +44,7 @@ class JibitException extends BankException
         'purchase.not_found' => 26,
         'purchase.invalid_state' => 27,
         'purchase.already_reversed' => 28,
+        'purchase.forbidden_card_number' => 29,
     );
 
     public const ERROR_MESSAGES = array(
@@ -76,5 +77,6 @@ class JibitException extends BankException
         'purchase.not_found' => 'سفارش یافت نشد.',
         'purchase.invalid_state' => 'وضعیت سفارش نامعتبر است',
         'purchase.already_reversed' => 'سفارش قبلاً برگردانده شده است.',
+        'purchase.forbidden_card_number' => 'کارت پرداخت کننده غیر مجاز است',
     );
 }
